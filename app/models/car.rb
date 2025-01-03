5d398151d4f8bfc439ca489a5298307eecfc08df
YanelyUc <yane.uc@gmail.com>
2025-01-03 16:27:28 -0600

    Create car model

---
+++ app/models/car.rb
@@ -0,0 +1,7 @@
+class Car < ApplicationRecord
+  START_DATE = 1900
+  validates :plate_number, presence: true, uniqueness: true
+  validates :year, presence: true, numericality: { only_integer: true,
+                                                   greater_than_or_equal_to: START_DATE,
+                                                   less_than_or_equal_to: Date.current.year }
+end
